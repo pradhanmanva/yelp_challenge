d04f549975320f1bdf60daba1d4712b19caf55e9
Manva Pradhan <pradhanmanva@gmail.com>
2017-11-30 20:21:09 -0600

    task-list

--- ann.py
+++ ann.py
@@ -4,8 +4,7 @@ import pandas as pd
 
 
 def find_checkins(id):
-    df = pd.read_csv(os.path.join("data", "biz_csv", 'business.csv'))
-    print(df)
-
+    df = pd.read_csv(os.path.join("data", "checkin_csv", 'checkin.csv'))
+    print(df.loc[df["business_id"] == id].sort_values(['no_of_checkins']))
 
 find_checkins(124296)
